77adab44b374c91bed669c25f988636ac7c3714c
Sergey <sabarovsky@yahoo.com>
2017-01-18 23:13:45 +0200

    started to learn html templates with jinja2

--- flask-micro-framework-introduction.py
+++ flask-micro-framework-introduction.py
@@ -2,12 +2,22 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+class User:
+    def __init__(self, first_name, last_name):
+        self.firstname = first_name
+        self.lastname = last_name
+
+    def initials(self):
+        return "{}. {}.".format(self.firstname[0], self.lastname[0])
+
 
 @app.route('/')
 @app.route('/index')
 
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', title='It title in py-file',
+                           user=User('Sergey', 'A')
+                           )
     # return 'Hi!'
 
 if __name__ == '__main__':
